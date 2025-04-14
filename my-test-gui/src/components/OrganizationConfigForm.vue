3ef461acbad425164802215f8fbfece812cd0bb0
Panagiotis <35231321+arstotzkan@users.noreply.github.com>
2025-04-14 22:59:30 +0300

    extract forms

---
+++ my-test-gui/src/components/OrganizationConfigForm.vue
@@ -0,0 +1,36 @@
+<script setup>
+</script>
+
+<template>
+  <form>
+    <div class="form-group mb-2">
+      <label for="organization">Organization</label>
+      <input class="form-control" id="organization">
+    </div>
+
+    <div class="form-group mb-2">
+      <label for="title">Title</label>
+      <input class="form-control" id="title">
+    </div>
+
+    <div class="form-group mb-2">
+      <label for="favouriteBrand">Favourite Brand</label>
+      <input class="form-control" id="favouriteBrand">
+    </div>
+
+    <div class="form-group mb-2">
+      <label for="mainButtonText">Main button text</label>
+      <input class="form-control" id="mainButtonText">
+    </div>
+
+    <div class="form-group mb-2">
+      <label for="helpURL">Help URL</label>
+      <input class="form-control" id="helpURL">
+    </div>
+
+    <div class="form-group">
+      <label for="helpText">Help Text</label>
+      <input class="form-control" id="helpText">
+    </div>
+  </form>
+</template>
