66c7af3283417f3e9e27aaa7fcc1bfe50e489f31
Panagiotis <35231321+arstotzkan@users.noreply.github.com>
2025-04-14 22:14:06 +0300

    add router

--- my-test-gui/src/router/index.js
+++ my-test-gui/src/router/index.js
@@ -1,14 +1,23 @@
 import { createRouter, createMemoryHistory } from "vue-router";
-import CheckSubconfigs from "../components/CheckSubconfigs.vue"
-import NotFound from "../components/NotFound.vue";
-import Preview from "../components/Preview.vue";
-import UpdateConfig from "../components/UpdateConfig.vue";
+import CountrySubconfigs from "../views/lists/CountrySubconfigs.vue";
+import LocationSubconfigs from "../views/lists/LocationSubconfigs.vue";
+import NotFound from "../views/NotFound.vue";
+import OrganizationConfigs from "../views/lists/OrganizationConfigs.vue";
+import UpdateConfig from "../views/UpdateConfig.vue";
 
 const routes = [
-    { path: '/', component: CheckSubconfigs },
-    { path: '/check-config', component: CheckSubconfigs },
-    { path: '/update-config', component: UpdateConfig },
-    { path: '/preview', component: Preview }, 
+    { path: '/', component: OrganizationConfigs },
+    { path: '/check-organization-config', component: CountrySubconfigs },
+    { path: '/check-organization-country-config', component: LocationSubconfigs },
+
+    { path: '/create-organization-config', component: UpdateConfig },
+    { path: '/create-organization-country-config', component: UpdateConfig },
+    { path: '/create-organization-location-config', component: UpdateConfig },
+
+    { path: '/update-organization-config', component: UpdateConfig },
+    { path: '/update-organization-country-config', component: UpdateConfig },
+    { path: '/update-organization-location-config', component: UpdateConfig },
+
 ]
   
 const router = createRouter({
