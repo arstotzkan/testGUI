2197bd544fd88ba716bbd63b84baea8de4f34dae
Panagiotis <35231321+arstotzkan@users.noreply.github.com>
2025-04-15 17:59:12 +0300

    add links to new pages to router

--- my-test-gui/src/router/index.js
+++ my-test-gui/src/router/index.js
@@ -7,6 +7,11 @@ import OrganizationConfigs from "../views/lists/OrganizationConfigs.vue";
 import UpdateOrganizationConfig from "../views/updateConfig/UpdateOrganizationConfig.vue";
 import UpdateCountryConfig from "../views/updateConfig/UpdateCountryConfig.vue";
 import UpdateLocationConfig from "../views/updateConfig/UpdateLocationConfig.vue";
+
+import CreateOrganizationConfig from "../views/createConfig/CreateOrganizationConfig.vue";
+import CreateCountryConfig from "../views/createConfig/CreateCountryConfig.vue";
+import CreateLocationConfig from "../views/createConfig/CreateLocationConfig.vue";
+
 import NotFound from "../views/NotFound.vue";
 
 const routes = [
@@ -14,9 +19,9 @@ const routes = [
     { path: '/check-organization-config', component: CountrySubconfigs },
     { path: '/check-organization-country-config', component: LocationSubconfigs },
 
-    // { path: '/create-organization-config', component: UpdateOrganizationConfig },
-    // { path: '/create-organization-country-config', component: UpdateCountryConfig },
-    // { path: '/create-organization-location-config', component: UpdateLocationConfig },
+    { path: '/create-organization-config', component: CreateOrganizationConfig },
+    { path: '/create-organization-country-config', component: CreateCountryConfig },
+    { path: '/create-organization-location-config', component: CreateLocationConfig },
 
     { path: '/update-organization-config', component: UpdateOrganizationConfig },
     { path: '/update-organization-country-config', component: UpdateCountryConfig },
