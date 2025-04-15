05a33b9ea6adb5f8333d247da821365e92dd6d41
Panagiotis <35231321+arstotzkan@users.noreply.github.com>
2025-04-15 13:58:23 +0300

    fix api calls

--- my-test-gui/src/services/userServices.js
+++ my-test-gui/src/services/userServices.js
@@ -1,25 +1,25 @@
 import api from './api';
 
-export function getOrganizationConfig(org) {
-    return api.get(`/api/get-organization-config?org=${org}`);
+export function getOrganizationConfig(id) {
+    return api.get(`/api/get-organization-config?id=${id}`);
 }
   
-export function getCountryConfig(org, country) {
-    return api.get(`/api/get-country-config?org=${org}&country=${country}`);
+export function getCountryConfig(id) {
+    return api.get(`/api/get-country-config?id=${id}`);
 }
 
-export function getLocationConfig(org, country, loc) {
-    return api.get(`/api/get-location-config?org=${org}&country=${country}&location=${loc}`);
+export function getLocationConfig(id) {
+    return api.get(`/api/get-location-config?id=${id}`);
 }
 
-export function getOrganizations(org) {
+export function getOrganizations() {
     return api.get(`/api/get-organizations`);
 }
   
-export function getOrganizationCountryConfigs(org) {
-    return api.get(`/api/get-organization-country-configs?org=${org}`);
+export function getOrganizationCountryConfigs(id) {
+    return api.get(`/api/get-organization-country-configs?id=${id}`);
 }
 
-export function getCountryLocationConfigs(org, country) {
-    return api.get(`/api/get-organization-location-configs?org=${org}&country=${country}`);
+export function getCountryLocationConfigs(id) {
+    return api.get(`/api/get-organization-location-configs?id=${id}`);
 }
